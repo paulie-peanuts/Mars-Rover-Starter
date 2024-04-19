50f3a565b58f69ccbfef460f57c5db42a673c79d
PauliePnuts <91554001+PauliePnuts@users.noreply.github.com>
2024-04-19 16:46:05 -0500

    cleaned up

--- rover.js
+++ rover.js
@@ -5,23 +5,11 @@ class Rover {
    constructor (position) {
       this.position = position;
       this.mode = 'NORMAL';
-      this.generatorWatts = 110
+      this.generatorWatts = 110;
    }
    receiveMessage(message) {
-      // console.log(message)
       let commandList = [];
-      // if (Array.isArray(message)) {
-      //    for (let i=0; i < message.length; i++) {
-      //       for (let command in message[i].commands) {
-      //          commandList.push(message[i].commands[command])
-      //       }
-      //    }
-      // } else {
-      //    for (let command in message.commands) {
-      //       commandList.push(message.commands[command])
-      //    }
-      // }
-      // console.log(commandList)
+
       for (let i=0; i<message.commands.length; i++) {
          if (message.commands[i].commandType === "STATUS_CHECK") {
             let object = {completed: true, roverStatus: {mode: this.mode, generatorWatts: this.generatorWatts, position: this.position}};
@@ -35,35 +23,18 @@ class Rover {
          if (message.commands[i].commandType === "MOVE") {
             if (this.mode === "NORMAL") {
                let object = {completed: true};
-               this.position = message.commands[i].value
-               // console.log("hi")
+               this.position = message.commands[i].value;
                commandList.push(object);
             } else {
-               commandList.push({completed:false})
+               commandList.push({completed:false});
             }
          }
       }
-
-      // if (message.commands.commandType.includes("STATUS_CHECK")) {
-         // console.log(`position: ${this.position}`)
-         // console.log(this.position)
-         // console.log(commandList)
-      // }
       return {
          "message": message.name,
          "results": commandList, 
       }
    }
 }
-// let command1 = {commandType: "STATUS_CHECK", value: 5};
-// let message1 = {name: "two words", commands: command1};
-// let fido = new Rover(55)
-// let response1 = fido.receiveMessage(message1)
-// console.log(response1.results)
-let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('MOVE', 10000)];
-let message = new Message('Test message with two commands', commands);
-let rover = new Rover(98382);    // Passes 98382 as the rover's position.
-let response = rover.receiveMessage(message);
-console.log(response);
-console.log(rover)
+
 module.exports = Rover;
